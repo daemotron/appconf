7cfabf0c4900bac07cd54d08cee3dea5c7075c77
daemotron <mail@daemotron.net>
2024-03-30 20:39:36 +0100

    refactor file name structure

---
+++ dirs_test.go
@@ -0,0 +1,90 @@
+package appconf_test
+
+import (
+	"github.com/daemotron/appconf"
+	"testing"
+)
+
+func setup() *appconf.AppConf {
+	return appconf.NewConf("apptest", appconf.WithAuthor("daemotron"), appconf.WithVersion("1.0"))
+}
+
+func TestUserDataDir(t *testing.T) {
+	app := setup()
+	_, err := app.UserDataDir()
+	if err != nil {
+		t.Fatalf(`Unexpected error: %v`, err)
+	}
+}
+
+func TestSiteDataDir(t *testing.T) {
+	app := setup()
+	_, err := app.SiteDataDir(false)
+	if err != nil {
+		t.Fatalf(`Unexpected error: %v`, err)
+	}
+}
+
+func TestGlobalDataDir(t *testing.T) {
+	app := setup()
+	_, err := app.GlobalDataDir()
+	if err != nil {
+		t.Fatalf(`Unexpected error: %v`, err)
+	}
+}
+
+func TestUserConfigDir(t *testing.T) {
+	app := setup()
+	_, err := app.UserConfigDir()
+	if err != nil {
+		t.Fatalf(`Unexpected error: %v`, err)
+	}
+}
+
+func TestSiteConfigDir(t *testing.T) {
+	app := setup()
+	_, err := app.SiteConfigDir(false)
+	if err != nil {
+		t.Fatalf(`Unexpected error: %v`, err)
+	}
+}
+
+func TestGlobalConfigDir(t *testing.T) {
+	app := setup()
+	_, err := app.GlobalConfigDir()
+	if err != nil {
+		t.Fatalf(`Unexpected error: %v`, err)
+	}
+}
+
+func TestUserStateDir(t *testing.T) {
+	app := setup()
+	_, err := app.UserStateDir()
+	if err != nil {
+		t.Fatalf(`Unexpected error: %v`, err)
+	}
+}
+
+func TestUserCacheDir(t *testing.T) {
+	app := setup()
+	_, err := app.UserCacheDir()
+	if err != nil {
+		t.Fatalf(`Unexpected error: %v`, err)
+	}
+}
+
+func TestGlobalCacheDir(t *testing.T) {
+	app := setup()
+	_, err := app.GlobalCacheDir()
+	if err != nil {
+		t.Fatalf(`Unexpected error: %v`, err)
+	}
+}
+
+func TestUserLogDir(t *testing.T) {
+	app := setup()
+	_, err := app.UserLogDir()
+	if err != nil {
+		t.Fatalf(`Unexpected error: %v`, err)
+	}
+}
