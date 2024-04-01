0db25c138dedbebe34bbbef0a75badbcf0dc9f49
daemotron <mail@daemotron.net>
2024-04-01 16:49:03 +0200

    Unit tests for data functions

---
+++ data_test.go
@@ -0,0 +1,99 @@
+package appconf_test
+
+import (
+	"github.com/daemotron/appconf"
+	"testing"
+)
+
+func TestStringValueToInt(t *testing.T) {
+	sv := appconf.StringValue("123")
+	intValue, err := sv.ToInt()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if intValue != 123 {
+		t.Errorf("StringValue.ToInt() = %d; expected %d", intValue, 123)
+	}
+}
+
+func TestStringValueToFloat64(t *testing.T) {
+	sv := appconf.StringValue("123.456")
+	float64Value, err := sv.ToFloat64()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if !appconf.AlmostEqual(float64Value, 123.456) {
+		t.Errorf("StringValue.ToFloat64() = %f; expected %f", float64Value, 123.456)
+	}
+}
+
+func TestStringValueToBool(t *testing.T) {
+	sv := appconf.StringValue("true")
+	boolValue, err := sv.ToBool()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if !boolValue {
+		t.Errorf("StringValue.ToBool() = %t; expected %t", boolValue, true)
+	}
+}
+
+func TestIntValueToFloat64(t *testing.T) {
+	iv := appconf.IntValue(456)
+	floatValue, err := iv.ToFloat64()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if !appconf.AlmostEqual(floatValue, 456.0) {
+		t.Errorf("IntValue.ToFloat64() = %f; expected %f", floatValue, 456.0)
+	}
+}
+
+func TestIntValueToBool(t *testing.T) {
+	iv := appconf.IntValue(0)
+	boolValue, err := iv.ToBool()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if boolValue {
+		t.Errorf("IntValue.ToBool() = %t; expected %t", boolValue, false)
+	}
+}
+
+func TestIntValueToString(t *testing.T) {
+	iv := appconf.IntValue(456)
+	stringValue := iv.ToString()
+	if stringValue != "456" {
+		t.Errorf("IntValue.ToString() = %s; expected %s", stringValue, "456")
+	}
+}
+
+func TestFloatValueToBool(t *testing.T) {
+	fv := appconf.FloatValue(0.0)
+	boolValue, err := fv.ToBool()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if boolValue != false {
+		t.Errorf("FloatValue.ToBool() = %t; expected %t", boolValue, false)
+	}
+}
+
+func TestFloatValueToInt(t *testing.T) {
+	fv := appconf.FloatValue(456.0)
+	intValue, err := fv.ToInt()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if intValue != 456 {
+		t.Errorf("FloatValue.ToBool() = %d; expected %d", intValue, 456)
+	}
+}
+
+func TestFloatValueToString(t *testing.T) {
+	fv := appconf.FloatValue(456.1)
+	stringValue := fv.ToString()
+	if stringValue != "456.1" {
+		t.Errorf("FloatValue.ToBool() = %s; expected %s", stringValue, "456.1")
+	}
+}
