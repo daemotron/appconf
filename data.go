0ceaf6059c975b569ce7b32659510896e4e488d1
daemotron <mail@daemotron.net>
2024-04-01 13:05:46 +0200

    improved type conversion safety

--- data.go
+++ data.go
@@ -13,39 +13,108 @@ import "strconv"
 // Please note: this is an abstract interface, use the type-specific
 // implementations to actually handle configuration values
 type Value interface {
-	String() string
+	ToString() string
+	ToInt() (int, error)
+	ToFloat64() (float64, error)
+	ToBool() (bool, error)
 }
 
 // A StringValue represents a string configuration value
 type StringValue string
 
-// String returns the string representation of the value.
-func (sv StringValue) String() string {
+// ToString returns the string representation of the value.
+func (sv StringValue) ToString() string {
 	return string(sv)
+}
+
+// ToInt returns the int representation of the value.
+func (sv StringValue) ToInt() (int, error) {
+	return strconv.Atoi(string(sv))
+}
+
+// ToFloat64 returns the float64 representation of the value.
+func (sv StringValue) ToFloat64() (float64, error) {
+	return strconv.ParseFloat(string(sv), 64)
+}
+
+// ToBool returns the bool representation of the value.
+func (sv StringValue) ToBool() (bool, error) {
+	return strconv.ParseBool(string(sv))
 }
 
 // An IntValue represents an int configuration value
 type IntValue int
 
-// String returns the string representation of the value
-func (iv IntValue) String() string {
+// ToString returns the string representation of the value
+func (iv IntValue) ToString() string {
 	return strconv.Itoa(int(iv))
+}
+
+// ToInt returns the int representation of the value.
+func (iv IntValue) ToInt() (int, error) {
+	return int(iv), nil
+}
+
+// ToFloat64 returns the float64 representation of the value.
+func (iv IntValue) ToFloat64() (float64, error) {
+	return float64(iv), nil
+}
+
+// ToBool returns the bool representation of the value.
+func (iv IntValue) ToBool() (bool, error) {
+	return iv != 0, nil
 }
 
 // A FloatValue represents a float64 configuration value
 type FloatValue float64
 
-// String returns the string representation of the value
-func (fv FloatValue) String() string {
+// ToString returns the string representation of the value
+func (fv FloatValue) ToString() string {
 	return strconv.FormatFloat(float64(fv), 'f', -1, 64)
+}
+
+// ToInt returns the int representation of the value.
+func (fv FloatValue) ToInt() (int, error) {
+	return int(fv), nil
+}
+
+// ToFloat64 returns the float64 representation of the value.
+func (fv FloatValue) ToFloat64() (float64, error) {
+	return float64(fv), nil
+}
+
+// ToBool returns the bool representation of the value.
+func (fv FloatValue) ToBool() (bool, error) {
+	return fv != 0, nil
 }
 
 // A BoolValue represents a bool configuration value
 type BoolValue bool
 
-// String returns the string representation of the value
-func (bv BoolValue) String() string {
+// ToString returns the string representation of the value
+func (bv BoolValue) ToString() string {
 	return strconv.FormatBool(bool(bv))
+}
+
+// ToInt returns the int representation of the value.
+func (bv BoolValue) ToInt() (int, error) {
+	if bv {
+		return 1, nil
+	}
+	return 0, nil
+}
+
+// ToFloat64 returns the float64 representation of the value.
+func (bv BoolValue) ToFloat64() (float64, error) {
+	if bv {
+		return 1, nil
+	}
+	return 0, nil
+}
+
+// ToBool returns the bool representation of the value.
+func (bv BoolValue) ToBool() (bool, error) {
+	return bool(bv), nil
 }
 
 // An Option represents a configuration option
